dc72a654da04f1a0b24d89705f0aba78ac10a651
BonsonW <bonson.ym@gmail.com>
2023-07-21 11:38:25 +1000

    removed curl submodule

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-/install/cmake-3.23.0-rc2-linux-x86_64/bin/cmake -DCMAKE_INSTALL_PREFIX=~/install -S . -B build
+cmake -DCMAKE_INSTALL_PREFIX=~/install -S . -B build
 cd build
 make clean
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 cd build
-./slow5-wget > log.txt
+./slow5-curl > log.txt
