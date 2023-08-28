6bda262b67725a97048c36da2ec032bbabd7f56b
Bonson Wong <boson.ym@gmail.com>
2023-08-28 12:36:28 +1000

    added test files

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+URL="https://gtgseq.s3.amazonaws.com/ont-r10-dna/NA12878/raw/PGXXHX230142_reads.blow5"
+IDX="test/raw/reads_10.blow5.idx"
+OUT="test/reads.blow5.idx"
+READ_LIST="test/raw/reads_10.txt"
+
 # terminate script
 die() {
 	echo "$1" >&2
@@ -7,7 +12,21 @@ die() {
 	exit 1
 }
 
-./tool test/example.blow5 > test/tmp.txt  || die "Running the tool failed"
-diff -q test/example.exp test/tmp.txt || die "diff failed"
+if [ "$1" = 'mem' ]; then
+    mem=1
+else
+    mem=0
+fi
+
+ex() {
+    if [ $mem -eq 1 ]; then
+        valgrind --leak-check=full --error-exitcode=1 "$@"
+    else
+        "$@"
+    fi
+}
+
+./slow5curl get -i ${IDX} -o ${OUT} "00002194-fea5-433c-ba89-1eb6b60f0f28"  || die "Running the tool failed"
+diff -q test/exp/reads_10.blow5 ${OUT} || die "diff failed"
 
 echo "Test passed"
