810485e76faf19ec2928597e0180a115d2c3685f
Bonson Wong <boson.ym@gmail.com>
2023-08-29 20:17:37 +1000

    updated config script

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-/install/cmake-3.23.0-rc2-linux-x86_64/bin/cmake -DCMAKE_INSTALL_PREFIX=~/install -S . -B build
+CMAKE="/install/cmake-3.23.0-rc2-linux-x86_64/bin/cmake"
+$CMAKE -DCMAKE_INSTALL_PREFIX=~/install -S . -B build
 cd build
 make clean
