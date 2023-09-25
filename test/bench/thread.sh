9f7a0bdd45ca4bbaeb86517fd4e649fd280dc349
Bonson Wong <boson.ym@gmail.com>
2023-09-25 16:29:53 +1000

    added thread benchmark script

---
+++ test/bench/thread.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# num_threads, list, url, region
+run () {
+    /usr/bin/time -v ./slow5curl get $3 -t $1 --list $2 2> "logs/[$4][t: $1].txt" -o reads.blow5
+}
+
+READS="test/data/raw/PGXX22394_reads_chr22_readid.list"
+URL="https://gtgseq.s3.amazonaws.com/ont-r10-dna/NA24385/raw/PGXX22394_reads.blow5"
+REGION=chr22
+
+for THREADS in 4 8 16 32 64 128 256 512
+do
+    run $THREADS $READS $URL $REGION
+done
