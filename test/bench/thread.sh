641f8a0e4b11d01325111a39d9a695f3d6b481fa
Bonson Wong <boson.ym@gmail.com>
2023-10-10 10:13:28 +1100

    fixed test script

--- test/bench/thread.sh
+++ test/bench/thread.sh
@@ -2,17 +2,29 @@
 
 # num_threads, list, url, name
 run () {
-    /usr/bin/time -v ./slow5curl get $3 -t $1 --list $2 --index "/home/hasindu/scratch/hg2_prom_lsk114/PGXX22394_reads.blow5.idx" 2> "logs/[$4 $5 local_idx][t: $1].txt" -o reads.blow5
+    /usr/bin/time -v ./slow5curl get $3 -t $1 --list $2 -o reads.blow5 --index $5 2> "logs/[$4 $6][t: $1].txt"
 }
 
-READS="/data/bonwon/slow5curl/test/data/raw/PGXX22394_reads_chr22_readid.list"
+READS="/data/bonwon/slow5curl/test/data/raw/PGXX22394_one_gene_readid.list"
 URL="https://gtgseq.s3.amazonaws.com/ont-r10-dna/NA24385/raw/PGXX22394_reads.blow5"
-NAME=chr22
+INDEX="/home/hasindu/scratch/hg2_prom_lsk114/PGXX22394_reads.blow5.idx"
+NAME=one
+NUM_RUNS=1
+ALL_THREADS=(
+    4
+    8
+    16
+    32
+    64
+    128
+    256
+    512
+)
 
-for N in {0..9}
+for i in $(seq 1 $NUM_RUNS)
 do
-    for THREADS in 128
+    for THREADS in ${ALL_THREADS[@]}
     do
-        run $THREADS $READS $URL $NAME $N
+        run $THREADS $READS $URL $NAME $INDEX $i
     done
 done
