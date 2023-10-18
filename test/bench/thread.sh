653ab2fd240fd664a8b67faf64572974412d93bb
Bonson Wong <boson.ym@gmail.com>
2023-10-18 14:20:55 +1100

    updated thread bench

--- test/bench/thread.sh
+++ test/bench/thread.sh
@@ -5,10 +5,10 @@ run () {
     /usr/bin/time -v ./slow5curl get $3 -t $1 --list $2 -o reads.blow5 --index $5 2> "logs/[$4 $6][t: $1].txt"
 }
 
-READS="/data/bonwon/slow5curl/test/data/raw/PGXX22394_one_gene_readid.list"
+READS="slow5curl/test/data/raw/PGXX22394_reads_chr22_readid.list"
 URL="https://gtgseq.s3.amazonaws.com/ont-r10-dna/NA24385/raw/PGXX22394_reads.blow5"
 INDEX="/home/hasindu/scratch/hg2_prom_lsk114/PGXX22394_reads.blow5.idx"
-NAME=one
+NAME=chr22
 NUM_RUNS=1
 ALL_THREADS=(
     4
